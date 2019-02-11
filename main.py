4ae0b1258f67a14a36978c98a9466465445f7d78
IngMaj <majic753@gmail.com>
2019-02-11 13:41:52 +0100

    Refaktorisanja...

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from sklearn.metrics import confusion_matrix
 import itertools
 
 
-
 def create_or_load_model(load_model_from_file=False):
     if load_model_from_file:
         print("Loading model...")
@@ -29,20 +28,20 @@ def create_or_load_model(load_model_from_file=False):
                             input_shape=(Constants.IMG_HEIGHT, Constants.IMG_WIDTH, 1), use_bias=False))
     model.add(Convolution2D(filters=64, kernel_size=(3, 3), activation="relu",use_bias=False))
     model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(BatchNormalization())
+    #model.add(BatchNormalization())
     model.add(Dropout(rate=0.25))
     model.add(Convolution2D(filters=128, kernel_size=(3, 3), activation="relu", use_bias=False))
     model.add(Convolution2D(filters=128, kernel_size=(3, 3), activation="relu", use_bias=False))
-    model.add(BatchNormalization())
+    #model.add(BatchNormalization())
     model.add(MaxPooling2D(pool_size=(2, 2)))
     model.add(Dropout(rate=0.25))
     model.add(Flatten())
-    model.add(Dense(units=4, activation="relu", use_bias=False))
+    model.add(Dense(units=36, activation="relu", use_bias=False))
     model.add(Dense(units=36, activation="relu", use_bias=False))
     model.add(Dense(units=Constants.NUM_CATEGORIES, activation="softmax", use_bias=False))
 
     model.summary()
-    model.compile(loss="categorical_crossentropy", optimizer=Adam(lr=0.001), metrics=["accuracy"])
+    model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
     return model
 
 
@@ -50,7 +49,8 @@ def show_image(image, color="gray"):
     plt.imshow(image, color)
     plt.show()
 
-#Argumenti su da li da ucitava model i koliko epoha da ga trenira
+
+# Argumenti su da li da ucitava model i koliko epoha da ga trenira
 def get_args_from_cmd():
     if len(sys.argv) != 3:
         return False, 5
@@ -103,48 +103,39 @@ def convert_to_num(y):
     return np.array(retval)
 
 
+def load_and_reshape_data(img_loader,set):
+    start_time = time.time()
+    print("Loading "+str(set) +" data...")
+    x, y = img_loader.load_set("test",normalize=True)
+    print(str(set)+' data loaded in: ',time.time()-start_time)
+    x = x.reshape(x.shape[0], Constants.IMG_HEIGHT, Constants.IMG_WIDTH, 1)
+    y = np_utils.to_categorical(y, Constants.NUM_CATEGORIES)
+    return x, y
+
+
 if __name__ == '__main__':
     load_model_from_file, num_epochs = get_args_from_cmd()
-    #Podesavanje modela (bazirano na vgg16)
     model = create_or_load_model(load_model_from_file)
-
     img_loader = ImageLoader()
-    start_time = time.time()
-    print("Loading training data...")
-    X_train, y_train = img_loader.load_set("train",normalize=True)
-    print('Training data loaded in: ',time.time()-start_time)
-    # Preprocesiranje, da radi sa tensorflowom, pre je bilo (X_train.shape[0], 224, 224)
-    X_train = X_train.reshape(X_train.shape[0], Constants.IMG_HEIGHT, Constants.IMG_WIDTH, 1)
-    # Preprocesiranje pretvara (0-normal,1-bacteria,2-virus) u [0.,0.,1.] [0.,1.,0.] i [1.,0.,0.]
-    y_train = np_utils.to_categorical(y_train, Constants.NUM_CATEGORIES)
-
+    X_train, y_train = load_and_reshape_data(img_loader, "train")
     print("Spliting training data to train and validation sets and adding small validation set..")
 
     X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train, test_size=0.2, random_state=None)
     # validacioni skup, nazvan small jer ima svega nekoliko slika
-    X_small,y_small = img_loader.load_set("val", normalize=True)
-    X_small = X_small.reshape(X_small.shape[0], Constants.IMG_HEIGHT, Constants.IMG_WIDTH, 1)
-    y_small = np_utils.to_categorical(y_small, Constants.NUM_CATEGORIES)
-    print(X_validate.shape)
-    print(X_small.shape)
+    X_small, y_small = load_and_reshape_data(img_loader, "val")
     X_validate = np.concatenate((X_validate, X_small), axis=0)
     y_validate = np.concatenate((y_validate,y_small), axis=0)
-    print(X_validate.shape)
     print("Done!")
 
-    """start_time = time.time()
-    print("Loading test data...")
-    X_test, y_test = img_loader.load_set("test",normalize=True)
-    print('Test data loaded in: ',time.time()-start_time)
-    X_test = X_test.reshape(X_test.shape[0], Constants.IMG_HEIGHT, Constants.IMG_WIDTH, 1)
-    y_test = np_utils.to_categorical(y_test, Constants.NUM_CATEGORIES)"""
+    #X_test, y_test = load_and_reshape_data(img_loader, "test")
 
     print("Training model...")
     model.fit(x=X_train, y=y_train, verbose=1, epochs=num_epochs)
     print("Saving model...")
     model.save(Constants.MODEL_FILE_PATH)
+
     print("Evaluating...")
-    loss, acc = model.evaluate(x=X_validate,y=y_validate,verbose=0)
+    loss, acc = model.evaluate(x=X_validate, y=y_validate, verbose=0)
     rounded_predictions = model.predict_classes(X_validate)
     cm = confusion_matrix(y_true=convert_to_num(y_validate), y_pred=rounded_predictions)
     plot_confusion_matrix(cm, Constants.CLASS_NAMES)
